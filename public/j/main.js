2a2fd84c06ae029645ea7a8617f48543e2d71a94
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-05-25 00:29:56 -0300

    refactor(public): add more examples

--- public/j/main.js
+++ public/j/main.js
@@ -4,3 +4,8 @@ import { OPERATIONS_REGISTER, PORTFOLIO_DISTRIBUTION } from './constants.js'
 const p = new MyStockPortfolio(OPERATIONS_REGISTER)
 p.portfolioDistribution = PORTFOLIO_DISTRIBUTION
 p.distributionForNewInvestment(10000)
+
+console.log(`Valor total investido: ${p.totalValueInvested()}\n\n`)
+console.log(`Retorno obtido com venda de posições: ${p.totalBalance()}\n\n`)
+console.log('Distribuição dos ativos:\n\n')
+p.actualDistribution()
