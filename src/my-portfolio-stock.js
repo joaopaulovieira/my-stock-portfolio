ba6aec7e107681735e0b6875de746588c62c4cb2
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-05-24 22:47:25 -0300

    feat(src): add possibility to calculate portfolio distribution for new investiment

--- src/my-portfolio-stock.js
+++ src/my-portfolio-stock.js
@@ -1,6 +1,12 @@
 export default class MyStockPortfolio {
+    get portfolioDistribution() { return this._portfolioDistribution }
+    set portfolioDistribution(distribution) { this._portfolioDistribution = distribution }
 
     constructor(rawOperations) {
     }
 
+    distributionForNewInvestment(value, distribution = this.portfolioDistribution) {
+        if (!distribution) return console.error(`No distribution parameter is received. Please, inform one distribution when calling this method or set a default distribution for this portfolio.`)
+        distribution.forEach(item => console.log(`${item.name} - ${((value * item.percentile)/100).toFixed(2)}`))
+    }
 }
