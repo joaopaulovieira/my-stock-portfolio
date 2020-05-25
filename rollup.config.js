b0b1a985a5afdecbe0963fb9562bc145bc46fc0d
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-05-25 00:55:19 -0300

    feat: add the possibility to use local private data
    Create a copy of the public/j/constants.js file and move it to the private_data folder. Then, fill the PORTFOLIO_DISTRIBUTION and OPERATIONS_REGISTER array with the data you needed.
    
    After that, just run locally the project with the start:use-private-data script.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
+import copy from 'rollup-plugin-copy'
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 import filesize from 'rollup-plugin-filesize'
@@ -12,17 +13,20 @@ import pkg from './package.json'
 let rollupConfig
 
 const serveLocal = !!process.env.SERVE
+const usePrivateData = !!process.env.PRIVATE_DATA
 const verboseBuild = !!process.env.VERBOSE_BUILD
 const visualizeBundle = !!process.env.GRAPH_ANALYZE_BUNDLE
 const minimize = !!process.env.MINIMIZE
 
 const babelPluginOptions = { exclude: 'node_modules/**', babelHelpers: 'bundled' }
+const copyPluginOptions = { targets: [{ src: 'private_data/constants.js', dest: 'public/j' }] }
 const servePluginOptions = { contentBase: ['dist', 'public'], host: '0.0.0.0', port: '8080' }
 const livereloadPluginOptions = { watch: ['dist', 'public'] }
 const terserPluginOptions = { include: [/^.+\.min\.js$/] }
 
 const plugins = [resolve(), commonjs(), babel(babelPluginOptions)]
 
+usePrivateData && plugins.push(copy(copyPluginOptions))
 verboseBuild && plugins.push(size(), filesize())
 serveLocal && plugins.push(serve(servePluginOptions), livereload(livereloadPluginOptions))
 visualizeBundle && plugins.push(visualize({ open: true }))
