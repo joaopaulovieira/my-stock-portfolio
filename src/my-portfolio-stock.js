896c7846bcc7aeb4491d348c1e2e89f16f9a9558
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-05-24 22:52:53 -0300

    feat(src): create insertadditionalData method
    Creates a method to insert one property (id) on each action object to differs between all registered actions.

--- src/my-portfolio-stock.js
+++ src/my-portfolio-stock.js
@@ -3,8 +3,18 @@ export default class MyStockPortfolio {
     set portfolioDistribution(distribution) { this._portfolioDistribution = distribution }
 
     constructor(rawOperations) {
+        this.operationsRegister = this.insertAdditionalMetadata(rawOperations)
+
     }
 
+
+    insertAdditionalMetadata(rawOperations) {
+        const updatedOperations = JSON.parse(JSON.stringify(rawOperations))
+        let id = 0
+        
+        updatedOperations.forEach(operation => (operation.id = ++id))
+        return updatedOperations
+    }
     distributionForNewInvestment(value, distribution = this.portfolioDistribution) {
         if (!distribution) return console.error(`No distribution parameter is received. Please, inform one distribution when calling this method or set a default distribution for this portfolio.`)
         distribution.forEach(item => console.log(`${item.name} - ${((value * item.percentile)/100).toFixed(2)}`))
